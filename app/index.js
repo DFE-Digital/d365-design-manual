dcd2a61094469941c69ec5feabcdcc43d03440a3
Eddie Haynes <77551941+haynes-dev@users.noreply.github.com>
2023-07-13 13:12:04 +0100

    x

--- app/index.js
+++ app/index.js
@@ -15,7 +15,7 @@ app.use(compression());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(favicon(path.join(__dirname, 'public/assets/images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../public/assets/images', 'favicon.ico')));
 
 app.set('view engine', 'html');
 
