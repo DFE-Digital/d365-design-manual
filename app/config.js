37ca1b6e70e64f13430d107146b8a0a7ed79055f
Eddie Haynes <77551941+haynes-dev@users.noreply.github.com>
2025-04-08 14:57:15 +0100

    Added comonents, styles and patterns plus layout revamp

--- app/config.js
+++ app/config.js
@@ -1,14 +1,9 @@
-module.exports = {
-
-    // Base URL
-    baseURL: process.env.BASE_URL || 'https://d365-design-manual-8523de7fad90.herokuapp.com/',
-  
-    // Environment
-    env: process.env.NODE_ENV || 'development',
-  
-    // Port to run local development server on
-    port: process.env.PORT || 3066,
-    githubrepo: 'https://github.com/haynes-dev/dfe-d365-design-manual',
-    
-    assetPath: process.env.assetPath
+const config = {
+  baseURL: process.env.BASE_URL || 'https://d365-design-manual-8523de7fad90.herokuapp.com/',
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3066,
+  githubrepo: 'https://github.com/haynes-dev/dfe-d365-design-manual',
+  assetPath: process.env.assetPath,
 };
+
+export default config;
---
+++ app/data/pages.js
@@ -0,0 +1,172 @@
+export default {
+  "home": {
+    title: "Dynamics 365 Design Manual",
+    url: "/",
+    description: "Principles, guidance, and standards to support people delivering joined-up, effective, user-centred outcomes for people who use Department for Education Dynamics 365 services."
+  },
+  "powerpages": {
+    title: "Power Pages",
+    url: "/powerpages/",
+    description: "Understand how we design consistent Power Pages services for sector users."
+  },
+  "powerpages/getting-started": {
+    title: "Getting started",
+    url: "/powerpages/getting-started/",
+    description: "Understand how to get started with building and designing your Power Pages website."
+  },
+  "powerpages/getting-started/branding-standards": {
+    title: "Branding standards",
+    url: "/powerpages/getting-started/branding-standards/",
+    description: "Learn about the branding standards for Power Pages websites."
+  },
+  "powerpages/getting-started/pre-requisites": {
+    title: "Pre-requisites",
+    url: "/powerpages/getting-started/pre-requisites/",
+    description: "Understand the pre-requisites before starting with Power Pages."
+  },
+  "powerpages/getting-started/set-up-your-custom-website": {
+    title: "Set up your custom website",
+    url: "/powerpages/getting-started/set-up-your-custom-website/",
+    description: "Guide to setting up your custom Power Pages website."
+  },
+  "powerpages/styles": {
+    title: "Styles",
+    url: "/powerpages/styles/",
+    description: "Understand the design standards and elements that create consistent, accessible Power Pages websites for sector users."
+  },
+  "powerpages/styles/layout": {
+    title: "Layout",
+    url: "/powerpages/styles/layout",
+    description: "Learn how to arrange content on your Power Pages website for effective, user-friendly layouts."
+  },
+  "powerpages/components": {
+    title: "Components",
+    url: "/powerpages/components/",
+    description: "Components are reusable parts of user interface that have been designed to specifically support your build."
+  },
+  "powerpages/components/attachment": {
+    title: "Attachment",
+    url: "/powerpages/components/attachment/",
+    description: "Use the attachment component to display downloadable file attachments (e.g. PDFs, Word documents) with a thumbnail, title, and metadata like file type or size."
+  },
+  "powerpages/components/card": {
+    title: "Card",
+    url: "/powerpages/components/card/",
+    description: "The card component is a versatile user interface element used to display grouped information in a visually appealing and organised way. They can enhance user interaction and content presentation across various applications and devices."
+  },
+  "powerpages/components/checkboxes": {
+    title: "Checkboxes",
+    url: "/powerpages/components/checkboxes/",
+    description: "Use the checkboxes component to assist users in selecting multiple options from a list or toggling a single option on or off. When submitting data to CRM, this component is suitable for boolean, select or text type target fields."
+  },
+  "powerpages/components/contents-list": {
+    title: "Contents list",
+    url: "/powerpages/components/contents-list/",
+    description: "Use a contents list at the top of a page to facilitate user navigation within a small group of related pages or page sections."
+  },
+  "powerpages/components/dashboard": {
+    title: "Dashboard",
+    url: "/powerpages/components/dashboard/",
+    description: "Use the dashboard component to present a summarised view of information for groups. This is particularly beneficial for segregating statuses within a large dataset or displaying financial transaction totals."
+  },
+  "powerpages/components/date-input": {
+    title: "Date input",
+    url: "/powerpages/components/date-input/",
+    description: "Use the date input component when users need to input a memorable or easily retrievable date. When submitting data to CRM, this component is suitable for date type target fields."
+  },
+  "powerpages/components/email-input": {
+    title: "Email input",
+    url: "/powerpages/components/email-input/",
+    description: "Use the email input component when allowing users to input their email address. When submitting data to CRM, this component is suitable for single line of text of type email target field."
+  },
+  "powerpages/components/number-input": {
+    title: "Number input",
+    url: "/powerpages/components/number-input/",
+    description: "Use the number input component when you need to let a user enter either a whole number, a decimal number or either. When submitting data to CRM, this can be used where the target field is an appropriate number type."
+  },
+  "powerpages/components/radios": {
+    title: "Radios",
+    url: "/powerpages/components/radios/",
+    description: "Use the radios component when users can only select one option from a list. This can be used when the target field is a lookup, select, boolean or text type."
+  },
+  "powerpages/components/search": {
+    title: "Search",
+    url: "/powerpages/components/search/",
+    description: "Use the search component when you need to let users search and select a record from a database table. e.g. A users might enter a Companies House Number to find an organisation. When submitting data to CRM, the target field should be a lookup to the selected record."
+  },
+  "powerpages/components/select": {
+    title: "Select",
+    url: "/powerpages/components/select/",
+    description: "The select component should only be used as a last resort in public-facing services because research shows that some users find selects very difficult to use. This can be used when the target field is a lookup, select, or text type."
+  },
+  "powerpages/components/sortable-table": {
+    title: "Sortable table",
+    url: "/powerpages/components/sortable-table/",
+    description: "Use the sortable table component to let users sort columns in ascending or descending order. This may be useful to help find data within a large table of data."
+  },
+  "powerpages/components/sortable-table": {
+    title: "Sortable table",
+    url: "/powerpages/components/sortable-table/",
+    description: "Use the sortable table component to let users sort columns in ascending or descending order. This may be useful to help find data within a large table of data."
+  },
+  "powerpages/components/telephone-input": {
+    title: "Telephone input",
+    url: "/powerpages/components/telephone-input/",
+    description: "Use the telephone input component when you need to let users enter a phone number. When submitting data to CRM, this can be used when the target field is a single line of text of type telephone."
+  },
+  "powerpages/components/text-input": {
+    title: "Text input",
+    url: "/powerpages/components/text-input/",
+    description: "Use the text input component when you need to let users enter text that's no longer than a single line. When submitting data to CRM, this can be used when the target field is a single line of text field, excluding email and telephone types, which have their own component examples."
+  },
+  "powerpages/components/textarea": {
+    title: "Textarea",
+    url: "/powerpages/components/textarea/",
+    description: "Use the textarea component when you need to let users enter an amount of text that's longer than a single line. When submitting data to CRM, this can be used when the target field is a multi-line text area type."
+  },
+  "powerpages/components/timeline": {
+    title: "Timeline",
+    url: "/powerpages/components/timeline/",
+    description: "Use the timeline component to help users understand what has happened and when. For example, a conversation history."
+  },
+  "powerpages/patterns": {
+    title: "Patterns",
+    url: "/powerpages/patterns/",
+    description: "Explore patterns that solve common design and development challenges when building Power Pages websites."
+  },
+  "powerpages/patterns/custom-errors": {
+    title: "Displaying custom errors",
+    url: "/powerpages/patterns/custom-errors/",
+    description: "Learn how to show helpful custom error messages to users on your Power Pages website."
+  },
+  "powerpages/patterns/back-link": {
+    title: "Overriding breadcrumbs with a back link",
+    url: "/powerpages/patterns/back-link/",
+    description: "Show a back link in place of breadcrumbs to simplify navigation in specific Power Pages journeys."
+  },
+  "powerpages/patterns/phase-banner": {
+    title: "Overriding the phase banner",
+    url: "/powerpages/patterns/phase-banner/",
+    description: "Find out how to replace or customise the phase banner to reflect your service's stage."
+  },
+  "powerpages/patterns/page-heading": {
+    title: "Setting the page heading",
+    url: "/powerpages/patterns/page-heading/",
+    description: "Understand how to apply consistent, accessible page headings across Power Pages screens."
+  },
+  "powerpages/patterns/caption-page-heading": {
+    title: "Using captions in page headings",
+    url: "/powerpages/patterns/caption-page-heading/",
+    description: "Enhance page context by adding captions above headings on Power Pages pages."
+  },
+  "powerpages/patterns/crm-helper-framework": {
+    title: "Validating and submitting data to CRM",
+    url: "/powerpages/patterns/crm-helper-framework/",
+    description: "Use the CRM Helper Framework to validate user input and submit data to Dynamics CRM."
+  },
+  "crm": {
+    title: "CRM",
+    url: "/crm/",
+    description: "Guide to CRM."
+  }
+};
